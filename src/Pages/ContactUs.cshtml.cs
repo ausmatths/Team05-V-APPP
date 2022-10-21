99f60c946c9991d7c22181d39218420223016237
Austin Matthews <amatthews2@seattleu.edu>
2022-10-21 16:31:28 -0700

    Added 'ILogger' interface for logging from 'ContactUsModel' in ContactUs.cshtml.cs

--- src/Pages/ContactUs.cshtml.cs
+++ src/Pages/ContactUs.cshtml.cs
@@ -1,10 +1,18 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
 
 namespace ContosoCrafts.WebSite.Pages
 {
     public class ContactUsModel : PageModel
     {
+        private readonly ILogger<ContactUsModel> _logger;
+
+        public ContactUsModel(ILogger<ContactUsModel> logger)
+        {
+            _logger = logger;
+        }
+
         public void OnGet()
         {
         }
