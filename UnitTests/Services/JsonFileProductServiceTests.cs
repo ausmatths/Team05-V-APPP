e7fb6d51a1b53412a4c42cac5275a5c08665414c
Austin Matthews <amatthews2@seattleu.edu>
2022-10-22 22:22:31 -0700

    Added Unit Testing for "JsonfileProductServiceTests.cs" under service folder.

--- UnitTests/Services/JsonFileProductServiceTests.cs
+++ UnitTests/Services/JsonFileProductServiceTests.cs
@@ -10,7 +10,6 @@ namespace UnitTests.Pages.Product.AddRating
 {
     public class JsonFileProductServiceTests
     {
-
         #region TestSetup
 
         [SetUp]
@@ -48,6 +47,30 @@ namespace UnitTests.Pages.Product.AddRating
         }
 
         [Test]
+        public void AddRating_InValid_Product_Empty_Should_Return_False()
+        {
+            // Arrange
+
+            // Act
+            var result = TestHelper.ProductService.AddRating("", 1);
+
+            // Assert
+            Assert.AreEqual(false, result);
+        }
+
+        [Test]
+        public void AddRating_Product_Does_Not_Exist_Should_Return_False()
+        {
+            // Arrange
+
+            // Act
+            var result = TestHelper.ProductService.AddRating("abcd", 1);
+
+            // Assert
+            Assert.AreEqual(false, result);
+        }
+
+        [Test]
         public void AddRating_Valid_Product_Rating_5_Should_Return_True()
         {
             // Arrange
@@ -65,7 +88,58 @@ namespace UnitTests.Pages.Product.AddRating
             Assert.AreEqual(countOriginal + 1, dataNewList.Ratings.Length);
             Assert.AreEqual(5, dataNewList.Ratings.Last());
         }
-        #endregion AddRating
 
+        [Test]
+        public void AddRating_Valid_Product_Rating_Less_Than_0_Should_Return_False()
+        {
+            // Arrange
+
+            // Get the First data item
+            var data = TestHelper.ProductService.GetAllData().First();
+            var countOriginal = data.Ratings.Length;
+
+            // Act
+            var result = TestHelper.ProductService.AddRating(data.Id, -1);
+
+            // Assert
+            Assert.AreEqual(false, result);
+        }
+
+        [Test]
+        public void AddRating_Valid_Product_Rating_Greater_Than_5_Should_Return_False()
+        {
+            // Arrange
+
+            // Get the First data item
+            var data = TestHelper.ProductService.GetAllData().First();
+            var countOriginal = data.Ratings.Length;
+
+            // Act
+            var result = TestHelper.ProductService.AddRating(data.Id, 6);
+
+            // Assert
+            Assert.AreEqual(false, result);
+        }
+
+        [Test]
+        public void AddRating_Product_Rating_Not_Exist_Should_Create_Array()
+        {
+            // Arrange
+
+            // Get the data item with null rating
+            var firstNullRatingData = TestHelper.ProductService.GetAllData().Where(a => a.Ratings == null).First();
+
+            // Act
+            var result = TestHelper.ProductService.AddRating(firstNullRatingData.Id, 5);
+            var newRatingData = TestHelper.ProductService.GetAllData().Where(a => a.Id == firstNullRatingData.Id).First();
+
+            // Assert
+            Assert.IsNull(firstNullRatingData.Ratings);
+            Assert.AreEqual(true, result);
+            Assert.AreEqual(1, newRatingData.Ratings.Length);
+            Assert.AreEqual(5, newRatingData.Ratings.First());
+        }
+
+        #endregion AddRating
     }
 }
