41279a82a9795e4511f61262b27e0cdebdb821f1
Priya Bhardwaj <pbhardwaj@seattleu.edu>
2022-10-24 14:59:53 -0700

    Added logger and product service in Create.cshtml.cs in folder Pages Product

--- src/Pages/Product/Create.cshtml.cs
+++ src/Pages/Product/Create.cshtml.cs
@@ -1,10 +1,23 @@
+using ContosoCrafts.WebSite.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
 
 namespace ContosoCrafts.WebSite.Pages.Product
 {
     public class CreateModel : PageModel
     {
+        private readonly ILogger<CreateModel> _logger;
+        public JsonFileProductService ProductService { get; }
+
+        public CreateModel(ILogger<CreateModel> logger,
+            JsonFileProductService productService)
+        {
+            _logger = logger;
+            ProductService = productService;
+
+        }
+
         public void OnGet()
         {
         }
