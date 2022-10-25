7dba320e3672eec6c58c59b3460fa047dbe09894
parth560 <114316487+parth560@users.noreply.github.com>
2022-10-24 21:54:19 -0700

    Added the "Update button" under Read.cshtml and linked prodect details with update page under Product folder.

--- src/Pages/Product/Update.cshtml.cs
+++ src/Pages/Product/Update.cshtml.cs
@@ -1,12 +1,49 @@
-using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc;
+
+using ContosoCrafts.WebSite.Models;
+using ContosoCrafts.WebSite.Services;
 
 namespace ContosoCrafts.WebSite.Pages.Product
 {
+    
     public class UpdateModel : PageModel
     {
-        public void OnGet()
+        
+        public JsonFileProductService ProductService { get; }
+
+        
+        /// <param name="logger"></param>
+        /// <param name="productService"></param>
+        public UpdateModel(JsonFileProductService productService)
         {
+            ProductService = productService;
+        }
+
+        // The data to show, bind to it for the post
+        [BindProperty]
+        public ProductModel Product { get; set; }
+
+       
+        /// <param name="id"></param>
+        public void OnGet(string id)
+        {
+            Product = ProductService.GetAllData().FirstOrDefault(m => m.Id.Equals(id));
+        }
+
+        
+        public IActionResult OnPost()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            ProductService.UpdateData(Product);
+
+            return RedirectToPage("./Index");
         }
     }
 }
