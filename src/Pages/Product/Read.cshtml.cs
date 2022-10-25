629448b55f8c12e9ceaf4332bb19c21366f3e281
ptaneti <ptaneti@seattleu.edu>
2022-10-24 19:57:00 -0700

    Added "Car details" to Pages/Product in Read.cshtml and Read.cshtml.cs

--- src/Pages/Product/Read.cshtml.cs
+++ src/Pages/Product/Read.cshtml.cs
@@ -1,16 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+﻿using System.Linq;
+
 using Microsoft.AspNetCore.Mvc.RazorPages;
+
+using ContosoCrafts.WebSite.Models;
+using ContosoCrafts.WebSite.Services;
 
 namespace ContosoCrafts.WebSite.Pages.Product
 {
     public class ReadModel : PageModel
     {
-        public void OnGet()
+        public JsonFileProductService ProductService { get; }
+
+        public ReadModel(JsonFileProductService productService)
         {
+            ProductService = productService;
+        }
+
+        public ProductModel Product;
+
+        public void OnGet(string id)
+        {
+            Product = ProductService.GetAllData().FirstOrDefault(m => m.Id.Equals(id));
         }
     }
 }
