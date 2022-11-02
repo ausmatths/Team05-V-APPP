f18d06319b1f5ad1497b5714070e6101070c65d1
Priya Bhardwaj <pbhardwaj@seattleu.edu>
2022-11-01 18:28:19 -0700

    Added comments to file ProductModel.cs

--- src/Models/ProductModel.cs
+++ src/Models/ProductModel.cs
@@ -5,27 +5,41 @@ using System.Collections.Generic;
 
 namespace ContosoCrafts.WebSite.Models
 {
+    /// <summary>
+    /// Products entered by the admin about the Product
+    /// </summary>
     public class ProductModel
     {
+        // The ID for this product, use a Guid so it is always unique
         public string Id { get; set; }
+
+        // Maker of Product
         public string Maker { get; set; }
 
+        // Image of product
         [JsonPropertyName("img")]
         public string Image { get; set; }
 
+        // Url of image
         public string Url { get; set; }
         
+        // Title of product
         [StringLength (maximumLength: 33, MinimumLength = 1, ErrorMessage = "The Title should have a length of more than {2} and less than {1}")]
         public string Title { get; set; }
 
+        // Description of product
         public string Description { get; set; }
 
+        // Ratings of product
         public int[] Ratings { get; set; }
 
+        // Type of product
         public ProductTypeEnum ProductType { get; set; } = ProductTypeEnum.Undefined;
 
+        // Quantity of product
         public string Quantity { get; set; }
 
+        // Price of product
         [Range (-1, 100, ErrorMessage = "Value for {0} must be between {1} and {2}.")]
         public int Price { get; set; }
 
