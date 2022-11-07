9b1e34f31dc594ac5b303e5781dd2c179ddac7ff
Priya Bhardwaj <pbhardwaj@seattleu.edu>
2022-11-07 13:07:41 -0800

    Added changes to ProductModel.Tests.cs

---
+++ UnitTests/Models/ProductModel.Tests.cs
@@ -0,0 +1,30 @@
+using System.Linq;
+
+using Microsoft.Extensions.Logging;
+
+using Moq;
+
+using NUnit.Framework;
+
+using ContosoCrafts.WebSite.Models;
+
+namespace UnitTests.Models.Product
+{
+    /// <summary>
+    /// Tests index page
+    /// </summary>
+    public class ProductModelTests
+    {
+        #region TestSetup
+
+        public static ProductModel pageModel;
+
+        [SetUp]
+        // Initialize Comment page state
+        public void TestInitialize()
+        {
+        }
+
+        #endregion TestSetup
+    }
+}
