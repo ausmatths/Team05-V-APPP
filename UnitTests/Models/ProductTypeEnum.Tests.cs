a3056a784ffc40f3a362ba6f2a1044f5dc58a3f6
Priya Bhardwaj <pbhardwaj@seattleu.edu>
2022-11-07 13:14:00 -0800

    Added changes to ProductTypeEnum.Tests.cs

--- UnitTests/Models/ProductTypeEnum.Tests.cs
+++ UnitTests/Models/ProductTypeEnum.Tests.cs
@@ -1,1 +1,19 @@
-﻿
+﻿using System.Linq;
+
+using Microsoft.Extensions.Logging;
+
+using Moq;
+
+using NUnit.Framework;
+
+using ContosoCrafts.WebSite.Models;
+
+namespace UnitTests.Models.ProductTypeEnum
+{
+    /// <summary>
+    /// Tests index page
+    /// </summary>
+    public enum ProductTypeEnumModelTests
+    {
+    }
+}
