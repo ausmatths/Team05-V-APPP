e4446857b74681ed332ea3b837b9c1c8c39260b4
Priya Bhardwaj <pbhardwaj@seattleu.edu>
2022-11-07 11:02:20 -0800

    Added comments to Program.cs

--- src/Program.cs
+++ src/Program.cs
@@ -3,6 +3,9 @@ using Microsoft.Extensions.Hosting;
 
 namespace ContosoCrafts.WebSite
 {
+    /// <summary>
+    /// This is the main program for stating the program
+    /// </summary>
     public class Program
     {
         public static void Main(string[] args)
