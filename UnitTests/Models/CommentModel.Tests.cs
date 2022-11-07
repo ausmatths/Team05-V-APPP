3c1db8d8eafbb09204a78600ead6bb95b25c9742
Priya Bhardwaj <pbhardwaj@seattleu.edu>
2022-11-07 13:06:31 -0800

    Added changes to CommentModel.Tests.cs

---
+++ UnitTests/Models/CommentModel.Tests.cs
@@ -0,0 +1,30 @@
+using System.Linq;
+
+using Microsoft.Extensions.Logging;
+
+using Moq;
+
+using NUnit.Framework;
+
+using ContosoCrafts.WebSite.Models;
+
+namespace UnitTests.Models.Comment
+{
+    /// <summary>
+    /// Tests index page
+    /// </summary>
+    public class CommentModelTests
+    {
+        #region TestSetup
+
+        public static CommentModel pageModel;
+
+        [SetUp]
+        // Initialize Comment page state
+        public void TestInitialize()
+        {
+        }
+
+        #endregion TestSetup
+    }
+}
