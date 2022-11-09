3899a1c3752caae6eaf649895a1a29baf043b6d8
Austin Matthews <amatthews2@seattleu.edu>
2022-11-08 19:52:27 -0800

    Added 'ContactUsTests' class and TestsSetup on ContactUsModel in 'UnitTests/Pages/ContactUs.cshtml.Tests.cs

--- UnitTests/Pages/ContactUs.cshtml.Tests.cs
+++ UnitTests/Pages/ContactUs.cshtml.Tests.cs
@@ -8,44 +8,28 @@ using NUnit.Framework;
 
 using ContosoCrafts.WebSite.Pages;
 
-namespace UnitTests.Pages.Index
+namespace UnitTests.Pages.ContactUs
 {
     /// <summary>
-    /// Tests index page
+    /// Tests ContctUs page
     /// </summary>
-    public class IndexTests
+    public class ContactUsTests
     {
         #region TestSetup
 
-        public static IndexModel pageModel;
+        public static ContactUsModel pageModel;
 
         [SetUp]
-        // Initialize index page state
+        // Initialize ContactUs page state
         public void TestInitialize()
         {
-            var MockLoggerDirect = Mock.Of<ILogger<IndexModel>>();
+            var MockLoggerDirect = Mock.Of<ILogger<ContactUsModel>>();
 
-            pageModel = new IndexModel(MockLoggerDirect, TestHelper.ProductService)
+            pageModel = new ContactUsModel(MockLoggerDirect, TestHelper.ProductService)
             {
             };
         }
 
         #endregion TestSetup
-
-        #region OnGet
-        [Test]
-        // OnGet method should return products
-        public void OnGet_Valid_Should_Return_Products()
-        {
-            // Arrange
-
-            // Act
-            pageModel.OnGet();
-
-            // Assert
-            Assert.AreEqual(true, pageModel.ModelState.IsValid);
-            Assert.AreEqual(true, pageModel.Products.ToList().Any());
-        }
-        #endregion OnGet
     }
 }
