5b6b50648be5964956a94acce81b07b7f7eeb253
parth560 <114316487+parth560@users.noreply.github.com>
2022-11-12 18:49:34 -0800

    Added Unit testing for ".razor components" file under componenets folder

--- UnitTests/Components/ProductList.razor.Tests.cs
+++ UnitTests/Components/ProductList.razor.Tests.cs
@@ -1,18 +1,14 @@
-using System.Linq;
-
-using Microsoft.Extensions.DependencyInjection;
 
 using Bunit;
 using NUnit.Framework;
 
 using ContosoCrafts.WebSite.Components;
+using Microsoft.Extensions.DependencyInjection;
 using ContosoCrafts.WebSite.Services;
+using System.Linq;
 
 namespace UnitTests.Components
 {
-    /// <summary>
-    /// Tests ProductList.razor page
-    /// </summary>
     public class ProductListTests : BunitTestContext
     {
         #region TestSetup
@@ -24,9 +20,6 @@ namespace UnitTests.Components
 
         #endregion TestSetup
 
-        /// <summary>
-        /// By default it should reneder the products
-        /// </summary>
         [Test]
         public void ProductList_Default_Should_Return_Content()
         {
@@ -40,7 +33,114 @@ namespace UnitTests.Components
             var result = page.Markup;
 
             // Assert
-            Assert.AreEqual(false, result.Contains("The Quantified Cactus: An Easy Plant Soil Moisture Sensor"));
+            Assert.AreEqual(true, result.Contains("Jeep Cherokee"));
         }
+
+        #region SelectProduct
+        [Test]
+        public void SelectProduct_Valid_ID_jenlooper_Should_Return_Content()
+        {
+            // Arrange
+            Services.AddSingleton<JsonFileProductService>(TestHelper.ProductService);
+            var id = "MoreInfoButton_Jeep_Grand_Cherokee";
+
+            var page = RenderComponent<ProductList>();
+
+            // Find the Buttons (more info)
+            var buttonList = page.FindAll("Button");
+
+            // Find the one that matches the ID looking for and click it
+            var button = buttonList.First(m => m.OuterHtml.Contains(id));
+
+            // Act
+            button.Click();
+
+            // Get the markup to use for the assert
+            var pageMarkup = page.Markup;
+
+            // Assert
+            Assert.AreEqual(true, pageMarkup.Contains("Jeep Cherokee"));
+        }
+        #endregion SelectProduct
+
+        #region SubmitRating
+
+        [Test]
+        public void SubmitRating_Valid_ID_Click_Unstared_Should_Increment_Count_And_Check_Star()
+        {
+            /*
+             This test tests that the SubmitRating will change the vote as well as the Star checked
+             Because the star check is a calculation of the ratings, using a record that has no stars and checking one makes it clear what was changed
+
+            The test needs to open the page
+            Then open the popup on the card
+            Then record the state of the count and star check status
+            Then check a star
+            Then check again the state of the cound and star check status
+
+            */
+
+            // Arrange
+            Services.AddSingleton<JsonFileProductService>(TestHelper.ProductService);
+            var id = "MoreInfoButton_Jeep_Wrangler_Unlimited";
+
+            var page = RenderComponent<ProductList>();
+
+            // Find the Buttons (more info)
+            var buttonList = page.FindAll("Button");
+
+            // Find the one that matches the ID looking for and click it
+            var button = buttonList.First(m => m.OuterHtml.Contains(id));
+            button.Click();
+
+            // Get the markup of the page post the Click action
+            var buttonMarkup = page.Markup;
+
+            // Get the Star Buttons
+            var starButtonList = page.FindAll("span");
+
+            // Get the Vote Count
+            // Get the Vote Count, the List should have 7 elements, element 2 is the string for the count
+            var preVoteCountSpan = starButtonList[1];
+            var preVoteCountString = preVoteCountSpan.OuterHtml;
+
+            // Get the First star item from the list, it should not be checked
+            var starButton = starButtonList.First(m => !string.IsNullOrEmpty(m.ClassName) && m.ClassName.Contains("fa fa-star"));
+
+            // Save the html for it to compare after the click
+            var preStarChange = starButton.OuterHtml;
+
+            // Act
+
+            // Click the star button
+            starButton.Click();
+
+            // Get the markup to use for the assert
+            buttonMarkup = page.Markup;
+
+            // Get the Star Buttons
+            starButtonList = page.FindAll("span");
+
+            // Get the Vote Count, the List should have 7 elements, element 2 is the string for the count
+            var postVoteCountSpan = starButtonList[1];
+            var postVoteCountString = postVoteCountSpan.OuterHtml;
+
+            // Get the Last stared item from the list
+            starButton = starButtonList.First(m => !string.IsNullOrEmpty(m.ClassName) && m.ClassName.Contains("fa fa-star checked"));
+
+            // Save the html for it to compare after the click
+            var postStarChange = starButton.OuterHtml;
+
+            // Assert
+
+            // Confirm that the record had no votes to start, and 1 vote after
+            Assert.AreEqual(true, preVoteCountString.Contains("Be the first to vote!"));
+            Assert.AreEqual(true, postVoteCountString.Contains("1 Vote"));
+            Assert.AreEqual(false, preVoteCountString.Equals(postVoteCountString));
+        }
+
+        
+        #endregion SubmitRating
+
     }
 }
