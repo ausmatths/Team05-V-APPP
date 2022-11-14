1813703e869bb0820dd568347b4adfdbd3cbc5fd
Priya Bhardwaj <pbhardwaj@seattleu.edu>
2022-11-13 22:02:42 -0800

    Added _Layout.cshtml.Tests.cs in Shared in Pages in UnitTests

---
+++ UnitTests/Pages/Shared/_Layout.cshtml.Tests.cs
@@ -0,0 +1,20 @@
+using System.Linq;
+
+using Microsoft.Extensions.Logging;
+
+using Moq;
+
+using NUnit.Framework;
+
+using ContosoCrafts.WebSite.Pages;
+
+namespace UnitTests.Pages.Shared._Layout
+{
+    /// <summary>
+    /// Tests ContctUs page
+    /// </summary>
+    public class _LayoutTests
+    {
+       
+    }
+}
