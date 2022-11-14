d964cdb2276b3960572dd3b257bc97a3c23e495e
ptaneti <ptaneti@seattleu.edu>
2022-11-13 23:07:26 -0800

    Added "Location.cshtml" Folder under "Pages" Folder

---
+++ src/Pages/Location.cshtml.cs
@@ -0,0 +1,31 @@
+using System.Collections.Generic;
+
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+using ContosoCrafts.WebSite.Models;
+using ContosoCrafts.WebSite.Services;
+
+namespace ContosoCrafts.WebSite.Pages
+{
+    public class LocationModel : PageModel
+    {
+        private readonly ILogger<LocationModel> _logger;
+
+        public LocationModel(ILogger<LocationModel> logger,
+            JsonFileProductService productService)
+        {
+            _logger = logger;
+            ProductService = productService;
+        }
+
+        public JsonFileProductService ProductService { get; }
+
+        public IEnumerable<ProductModel> Products { get; private set; }
+
+        public void OnGet()
+        {
+            Products = ProductService.GetAllData();
+        }
+    }
+}
