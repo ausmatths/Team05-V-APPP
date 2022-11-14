c94e186bf43ffb2eabaad7fec0896fc3579d6180
Priya Bhardwaj <pbhardwaj@seattleu.edu>
2022-11-13 21:13:14 -0800

    Added new ProductDisplay.cshtml.cs page in Pages

---
+++ src/Pages/ProductDisplay.cshtml.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+using ContosoCrafts.WebSite.Models;
+using ContosoCrafts.WebSite.Services;
+
+namespace ContosoCrafts.WebSite.Pages
+{
+    /// <summary>
+    /// pranavi taneti
+    /// </summary>
+    public class ProductDisplayModel : PageModel
+    {
+
+        private readonly ILogger<IndexModel> _logger;
+
+        public ProductDisplayModel(ILogger<IndexModel> logger,
+            JsonFileProductService productService)
+        {
+            _logger = logger;
+            ProductService = productService;
+        }
+
+        public JsonFileProductService ProductService { get; }
+        public IEnumerable<ProductModel> Products { get; private set; }
+
+        public void OnGet()
+        {
+            Products = ProductService.GetAllData();
+        }
+    }
+}
