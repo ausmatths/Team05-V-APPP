4cf693ba2aace129125e4c3ab2ed22bb9dd90055
Austin Matthews <amatthews2@seattleu.edu>
2022-11-15 14:22:36 -0800

    Added "ReviewModel" interface 'ILogger' in Review.cshtml.cs

--- src/Pages/Review.cshtml.cs
+++ src/Pages/Review.cshtml.cs
@@ -1,1 +1,19 @@
-﻿
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace ContosoCrafts.WebSite.Pages
+{
+	public class ReviewModel : PageModel
+	{
+		private readonly ILogger<ReviewModel> _logger;
+
+		public ReviewModel(ILogger<ReviewModel> logger)
+		{
+			_logger = logger;
+		}
+
+		public void OnGet()
+		{
+		}
+	}
+}
