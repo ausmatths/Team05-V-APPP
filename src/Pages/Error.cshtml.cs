c6e628e550eb116b4c5a69f6f6e97ca15d8953fa
Priya Bhardwaj <pbhardwaj@seattleu.edu>
2022-11-15 21:52:45 -0800

    Added comments to Error.cshtml.cs

--- src/Pages/Error.cshtml.cs
+++ src/Pages/Error.cshtml.cs
@@ -6,11 +6,20 @@ using Microsoft.Extensions.Logging;
 
 namespace ContosoCrafts.WebSite.Pages
 {
+    /// <summary>
+    /// Error detection model 
+    /// </summary>
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public class ErrorModel : PageModel
     {
+        /// <summary>
+        /// Request id getter setter
+        /// </summary>
         public string RequestId { get; set; }
 
+        /// <summary>
+        /// ShowRequestId checks if string is empty or not
+        /// </summary>
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
 
         private readonly ILogger<ErrorModel> _logger;
@@ -20,6 +29,9 @@ namespace ContosoCrafts.WebSite.Pages
             _logger = logger;
         }
 
+        /// <summary>
+        /// OnGet gets Activity.Current's ID
+        /// </summary>
         public void OnGet()
         {
             RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier;
