d3170fe681f7fe06d87aa47652821b1c2fbddfc2
Priya Bhardwaj <pbhardwaj@seattleu.edu>
2022-11-15 21:40:05 -0800

    Added comments to ProductsController.cs file

--- src/Controllers/ProductsController.cs
+++ src/Controllers/ProductsController.cs
@@ -7,6 +7,9 @@ using ContosoCrafts.WebSite.Services;
 
 namespace ContosoCrafts.WebSite.Controllers
 {
+    /// <summary>
+    /// Controllers for containing the flow control logic
+    /// </summary>
     [ApiController]
     [Route("[controller]")]
     public class ProductsController : ControllerBase
@@ -18,12 +21,21 @@ namespace ContosoCrafts.WebSite.Controllers
 
         public JsonFileProductService ProductService { get; }
 
+        /// <summary>
+        /// When called, gets all data from products
+        /// </summary>
+        /// <returns></returns>
         [HttpGet]
         public IEnumerable<ProductModel> Get()
         {
             return ProductService.GetAllData();
         }
 
+        /// <summary>
+        /// Applies partial modifications rating to the product
+        /// </summary>
+        /// <param name="request"></param>
+        /// <returns></returns>
         [HttpPatch]
         public ActionResult Patch([FromBody] RatingRequest request)
         {
@@ -32,6 +44,10 @@ namespace ContosoCrafts.WebSite.Controllers
             return Ok();
         }
 
+        /// <summary>
+        /// Gets and sets product id to system generated id
+        /// and contains getter setter for rating
+        /// </summary>
         public class RatingRequest
         {
             public string ProductId { get; set; } = System.Guid.NewGuid().ToString();
