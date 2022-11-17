92783b73f2466ae20cfb676d7c4b1d7081d57db7
Priya Bhardwaj <pbhardwaj@seattleu.edu>
2022-11-17 14:20:13 -0800

    Added comments to ProductModel.cs

--- src/Models/ProductModel.cs
+++ src/Models/ProductModel.cs
@@ -10,44 +10,68 @@ namespace ContosoCrafts.WebSite.Models
     /// </summary>
     public class ProductModel
     {
-        // The ID for this product, use a Guid so it is always unique
+        /// <summary>
+        /// The ID for this product, use a Guid so it is always unique, getter, setter
+        /// </summary>
         public string Id { get; set; }
 
-        // Maker of Product
+        /// <summary>
+        /// getter, setter for maker of Product
+        /// </summary>
         public string Maker { get; set; }
 
-        // Image of product
+        /// <summary>
+        /// getter, setter for image of product
+        /// </summary>
         [JsonPropertyName("img")]
         public string Image { get; set; }
 
-        // Url of image
+        /// <summary>
+        /// getter, setter for Url of image
+        /// </summary>
         public string Url { get; set; }
-        
-        // Title of product
+
+        /// <summary>
+        /// getter, setter for Title of product
+        /// </summary>
         [StringLength (maximumLength: 33, MinimumLength = 1, ErrorMessage = "The Title should have a length of more than {2} and less than {1}")]
         public string Title { get; set; }
 
-        // Description of product
+        /// <summary>
+        /// getter, setter for Description of product
+        /// </summary>
         public string Description { get; set; }
 
-        // Ratings of product
+        /// <summary>
+        /// getter, setter for Ratings of product
+        /// </summary>
         public int[] Ratings { get; set; }
 
-        // Type of product
+        /// <summary>
+        /// getter, setter for Type of product
+        /// </summary>
         public ProductTypeEnum ProductType { get; set; } = ProductTypeEnum.Undefined;
 
-        // Quantity of product
+        /// <summary>
+        /// getter, setter for quantity
+        /// </summary>
         public string Quantity { get; set; }
 
-        // Price of product
+        /// <summary>
+        /// getter, setter for Price of product
+        /// </summary>
         [Range (-1, 100, ErrorMessage = "Value for {0} must be between {1} and {2}.")]
         public int Price { get; set; }
 
-        // Store the Comments entered by the users on this product
+        /// <summary>
+        /// getter, setter for Store the Comments entered by the users on this product
+        /// </summary>
         public List<CommentModel> CommentList { get; set; } = new List<CommentModel>();
 
+        /// <summary>
+        /// Serializes products
+        /// </summary>
+        /// <returns></returns>
         public override string ToString() => JsonSerializer.Serialize<ProductModel>(this);
-
-
     }
 }
