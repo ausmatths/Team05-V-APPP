811d8c3e1d5b2b3037e92a66173765fcaf2c24e2
Priya Bhardwaj <pbhardwaj@seattleu.edu>
2022-11-17 16:15:44 -0800

    Added comments to Create.cshtml.cs

--- src/Pages/Product/Create.cshtml.cs
+++ src/Pages/Product/Create.cshtml.cs
@@ -2,34 +2,42 @@ using ContosoCrafts.WebSite.Models;
 using ContosoCrafts.WebSite.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.Extensions.Logging;
+
 namespace ContosoCrafts.WebSite.Pages.Product
 {
     /// <summary>
     /// Implementing create for creating a new product
     /// </summary>
-
     public class CreateModel : PageModel
     {
-        private readonly ILogger<CreateModel> _logger;
+        /// <summary>
+        /// Getter for ProductService 
+        /// </summary>
         public JsonFileProductService ProductService { get; }
 
+        /// <summary>
+        /// getter setter for product
+        /// </summary>
         public ProductModel product { get; private set; }
 
-        // Linking to ProductService
+        /// <summary>
+        /// Linking to ProductService
+        /// </summary>
+        /// <param name="productService"></param>
         public CreateModel(JsonFileProductService productService)
         {
             ProductService = productService;
         }
 
-        // Posts the product and redirects to index page of product
+        /// <summary>
+        /// Posts the product and redirects to index page of product
+        /// </summary>
+        /// <param name="product"></param>
+        /// <returns></returns>
         public IActionResult OnPost(ProductModel product)
         {
             ProductService.CreateData(product);
             return RedirectToPage("./Index");
-        }
-        public void OnGet()
-        {
         }
     }
 }
