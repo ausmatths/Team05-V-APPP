d5cf531d8fbe110e323629271cfd7b4592fae311
Priya Bhardwaj <pbhardwaj@seattleu.edu>
2022-11-17 16:44:24 -0800

    Added comments to Payment.cshtml.cs

--- src/Pages/Payment.cshtml.cs
+++ src/Pages/Payment.cshtml.cs
@@ -3,15 +3,26 @@ using Microsoft.Extensions.Logging;
 
 namespace ContosoCrafts.WebSite.Pages
 {
+    /// <summary>
+    /// Payment page
+    /// </summary>
     public class PaymentModel : PageModel
     {
+        // Variable for logger
         private readonly ILogger<PaymentModel> _logger;
 
+        /// <summary>
+        /// Constructor for PaymentModel
+        /// </summary>
+        /// <param name="logger"></param>
         public PaymentModel(ILogger<PaymentModel> logger)
         {
             _logger = logger;
         }
 
+        /// <summary>
+        /// OnGet for payments
+        /// </summary>
         public void OnGet()
         {
         }
