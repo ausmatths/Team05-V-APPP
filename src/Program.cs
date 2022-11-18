df1ace4f5e2b49d05b780ce3e8279e295928f1c3
Priya Bhardwaj <pbhardwaj@seattleu.edu>
2022-11-17 17:03:36 -0800

    Added comments to Program.cs

--- src/Program.cs
+++ src/Program.cs
@@ -8,11 +8,20 @@ namespace ContosoCrafts.WebSite
     /// </summary>
     public class Program
     {
+        /// <summary>
+        /// Main method to run the program
+        /// </summary>
+        /// <param name="args"></param>
         public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
         }
 
+        /// <summary>
+        /// Creates a HostBuilder
+        /// </summary>
+        /// <param name="args"></param>
+        /// <returns></returns>
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
