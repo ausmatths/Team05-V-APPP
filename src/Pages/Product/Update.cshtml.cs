4999b477f5e057eb40d9b98f20e95d2e3c62f461
Priya Bhardwaj <pbhardwaj@seattleu.edu>
2022-11-17 16:26:55 -0800

    Added comments to Update.cshtml.cs

--- src/Pages/Product/Update.cshtml.cs
+++ src/Pages/Product/Update.cshtml.cs
@@ -1,8 +1,6 @@
 using System.Linq;
-
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc;
-
 using ContosoCrafts.WebSite.Models;
 using ContosoCrafts.WebSite.Services;
 
@@ -11,30 +9,41 @@ namespace ContosoCrafts.WebSite.Pages.Product
     /// <summary>
     ///  Update Page will let the user update the existing data
     /// </summary>
-
     public class UpdateModel : PageModel
     {
-        
+        /// <summary>
+        /// Getter for ProductService 
+        /// </summary>
         public JsonFileProductService ProductService { get; }
-    
-        /// <param name="logger"></param>
+
+        /// <summary>
+        /// Constructor for UpdateModel
+        /// </summary>
         /// <param name="productService"></param>
         public UpdateModel(JsonFileProductService productService)
         {
             ProductService = productService;
         }
 
-        // The data to show, bind to it for the post
+        /// <summary>
+        /// The data to show, bind to it for the post
+        /// </summary>
         [BindProperty]
         public ProductModel Product { get; set; }
        
+        /// <summary>
+        /// OnGet gets frist data or default from products
+        /// </summary>
         /// <param name="id"></param>
         public void OnGet(string id)
         {
             Product = ProductService.GetAllData().FirstOrDefault(m => m.Id.Equals(id));
         }
 
-        // Posts the data in product and redirects to product index page
+        /// <summary>
+        /// Posts the data in product and redirects to product index page
+        /// </summary>
+        /// <returns></returns>
         public IActionResult OnPost()
         {
             if (!ModelState.IsValid)
