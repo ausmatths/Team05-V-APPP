dbd074f8de04a62560a91792b0df93936af80261
Priya Bhardwaj <pbhardwaj@seattleu.edu>
2022-11-17 19:13:44 -0800

    Added comments to ProductList.razor.Tests.cs

--- UnitTests/Components/ProductList.razor.Tests.cs
+++ UnitTests/Components/ProductList.razor.Tests.cs
@@ -1,6 +1,5 @@
 using Bunit;
 using NUnit.Framework;
-
 using ContosoCrafts.WebSite.Components;
 using Microsoft.Extensions.DependencyInjection;
 using ContosoCrafts.WebSite.Services;
@@ -8,10 +7,16 @@ using System.Linq;
 
 namespace UnitTests.Components
 {
+    /// <summary>
+    /// Testing ProductList
+    /// </summary>
     public class ProductListTests : BunitTestContext
     {
         #region TestSetup
 
+        /// <summary>
+        /// Test Initialize for ProductList
+        /// </summary>
         [SetUp]
         public void TestInitialize()
         {
@@ -19,114 +24,127 @@ namespace UnitTests.Components
 
         #endregion TestSetup
 
+        #region DefaultProduct
+
+        /// <summary>
+        /// ProductList_Default_Should_Return_Content
+        /// </summary>
         [Test]
         public void ProductList_Default_Should_Return_Content()
         {
-            /// Arrange
+            // Arrange
             Services.AddSingleton<JsonFileProductService>(TestHelper.ProductService);
 
-            /// Act
+            // Act
             var page = RenderComponent<ProductList>();
 
-            /// Get the Cards retrned
+            // Get the Cards retrned
             var result = page.Markup;
 
-            /// Assert
+            // Assert
             Assert.AreEqual(true, result.Contains("Jeep Cherokee"));
         }
 
+        #endregion DefaultProduct
+
         #region SelectProduct
+
+        /// <summary>
+        /// SelectProduct_Valid_ID_jenlooper_Should_Return_Content
+        /// </summary>
         [Test]
         public void SelectProduct_Valid_ID_jenlooper_Should_Return_Content()
         {
-            /// Arrange
+            // Arrange
             Services.AddSingleton<JsonFileProductService>(TestHelper.ProductService);
             var id = "MoreInfoButton_Jeep_Grand_Cherokee";
 
             var page = RenderComponent<ProductList>();
 
-            /// Find the Buttons (more info)
+            // Find the Buttons (more info)
             var buttonList = page.FindAll("Button");
 
-            /// Find the one that matches the ID looking for and click it
+            // Find the one that matches the ID looking for and click it
             var button = buttonList.First(m => m.OuterHtml.Contains(id));
 
-            /// Act
+            // Act
             button.Click();
 
-            /// Get the markup to use for the assert
+            // Get the markup to use for the assert
             var pageMarkup = page.Markup;
 
-            /// Assert
+            // Assert
             Assert.AreEqual(true, pageMarkup.Contains("Jeep Cherokee"));
         }
         #endregion SelectProduct
 
         #region SubmitRating
 
+        /// <summary>
+        /// SubmitRating_Valid_ID_Click_Unstared_Should_Increment_Count_And_Check_Star
+        /// </summary>
         [Test]
         public void SubmitRating_Valid_ID_Click_Unstared_Should_Increment_Count_And_Check_Star()
         {
             
-            /// Arrange
+            // Arrange
             Services.AddSingleton<JsonFileProductService>(TestHelper.ProductService);
             var id = "MoreInfoButton_Jeep_Wrangler_Unlimited";
 
             var page = RenderComponent<ProductList>();
 
-            /// Find the Buttons (more info)
+            // Find the Buttons (more info)
             var buttonList = page.FindAll("Button");
 
-            /// Find the one that matches the ID looking for and click it
+            // Find the one that matches the ID looking for and click it
             var button = buttonList.First(m => m.OuterHtml.Contains(id));
             button.Click();
 
-            /// Get the markup of the page post the Click action
+            // Get the markup of the page post the Click action
             var buttonMarkup = page.Markup;
 
-            /// Get the Star Buttons
+            // Get the Star Buttons
             var starButtonList = page.FindAll("span");
 
-            /// Get the Vote Count
-            /// Get the Vote Count, the List should have 7 elements, element 2 is the string for the count
+            // Get the Vote Count
+            // Get the Vote Count, the List should have 7 elements, element 2 is the string for the count
             var preVoteCountSpan = starButtonList[1];
             var preVoteCountString = preVoteCountSpan.OuterHtml;
 
-            /// Get the First star item from the list, it should not be checked
+            // Get the First star item from the list, it should not be checked
             var starButton = starButtonList.First(m => !string.IsNullOrEmpty(m.ClassName) && m.ClassName.Contains("fa fa-star"));
 
-            /// Save the html for it to compare after the click
+            // Save the html for it to compare after the click
             var preStarChange = starButton.OuterHtml;
 
-            /// Act
+            // Act
 
-            /// Click the star button
+            // Click the star button
             starButton.Click();
 
-            /// Get the markup to use for the assert
+            // Get the markup to use for the assert
             buttonMarkup = page.Markup;
 
-            /// Get the Star Buttons
+            // Get the Star Buttons
             starButtonList = page.FindAll("span");
 
-            /// Get the Vote Count, the List should have 7 elements, element 2 is the string for the count
+            // Get the Vote Count, the List should have 7 elements, element 2 is the string for the count
             var postVoteCountSpan = starButtonList[1];
             var postVoteCountString = postVoteCountSpan.OuterHtml;
 
-            /// Get the Last stared item from the list
+            // Get the Last stared item from the list
             starButton = starButtonList.First(m => !string.IsNullOrEmpty(m.ClassName) && m.ClassName.Contains("fa fa-star checked"));
 
-            /// Save the html for it to compare after the click
+            // Save the html for it to compare after the click
             var postStarChange = starButton.OuterHtml;
 
-            /// Assert
+            // Assert
 
-            /// Confirm that the record had no votes to start, and 1 vote after
+            // Confirm that the record had no votes to start, and 1 vote after
             Assert.AreEqual(true, preVoteCountString.Contains("Be the first to vote!"));
             Assert.AreEqual(true, postVoteCountString.Contains("1 Vote"));
             Assert.AreEqual(false, preVoteCountString.Equals(postVoteCountString));
         }      
         #endregion SubmitRating
-
     }
 }
