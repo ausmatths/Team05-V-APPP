724b6f3a084fb16131e2c14fb8cad012c18899bf
Priya Bhardwaj <pbhardwaj@seattleu.edu>
2022-11-17 17:52:11 -0800

    Added comments to TestFixture.cs

--- UnitTests/TestFixture.cs
+++ UnitTests/TestFixture.cs
@@ -1,6 +1,6 @@
 using System.IO;
-
 using NUnit.Framework;
+using NUnit.Framework.Internal;
 
 namespace UnitTests
 {
@@ -16,22 +16,23 @@ namespace UnitTests
         // Path to the data folder for the content
         public static string DataContentRootPath = "./data/";
 
-        [OneTimeSetUp]
+        /// <summary>
+        /// Run this code once when the test harness starts up.
+        /// This will copy over the latest version of the database files
+        /// C:\repos\5110\ClassBaseline\UnitTests\bin\Debug\net5.0\wwwroot\data
+        /// C:\repos\5110\ClassBaseline\src\wwwroot\data
+        /// C:\repos\5110\ClassBaseline\src\bin\Debug\net5.0\wwwroot\data
+        /// </summary>
+       [OneTimeSetUp]
         public void RunBeforeAnyTests()
         {
-            // Run this code once when the test harness starts up.
-
-            // This will copy over the latest version of the database files
-
-            // C:\repos\5110\ClassBaseline\UnitTests\bin\Debug\net5.0\wwwroot\data
-            // C:\repos\5110\ClassBaseline\src\wwwroot\data
-            // C:\repos\5110\ClassBaseline\src\bin\Debug\net5.0\wwwroot\data
-
-
-
             // var DataWebPath = "../../../../src/bin/Debug/net5.0/wwwroot/data";
             var DataWebPath = "../../../../src/wwwroot/data";
+
+            // var for DataUTDirectory
             var DataUTDirectory = "wwwroot";
+
+            // var for DataUTPath 
             var DataUTPath = DataUTDirectory + "/data";
 
             // Delete the Detination folder
@@ -47,13 +48,19 @@ namespace UnitTests
             var filePaths = Directory.GetFiles(DataWebPath);
             foreach (var filename in filePaths)
             {
+                // saves the name of the file
                 string OriginalFilePathName = filename.ToString();
+
+                // saves the original file path name
                 var newFilePathName = OriginalFilePathName.Replace(DataWebPath, DataUTPath);
 
                 File.Copy(OriginalFilePathName, newFilePathName);
             }
         }
 
+        /// <summary>
+        /// To be run after any test
+        /// </summary>
         [OneTimeTearDown]
         public void RunAfterAnyTests()
         {
