03024bdb24b8d0268f4eef24d985ae534f56a5d9
Priya Bhardwaj <pbhardwaj@seattleu.edu>
2022-11-17 16:48:44 -0800

    Added comments to Privacy.cshtml.cs

--- src/Pages/Privacy.cshtml.cs
+++ src/Pages/Privacy.cshtml.cs
@@ -3,15 +3,26 @@ using Microsoft.Extensions.Logging;
 
 namespace ContosoCrafts.WebSite.Pages
 {
+    /// <summary>
+    /// Privacy page fro privacy description
+    /// </summary>
     public class PrivacyModel : PageModel
     {
+        // Variable for logger
         private readonly ILogger<PrivacyModel> _logger;
 
+        /// <summary>
+        /// Constructor for PrivacyModel
+        /// </summary>
+        /// <param name="logger"></param>
         public PrivacyModel(ILogger<PrivacyModel> logger)
         {
             _logger = logger;
         }
 
+        /// <summary>
+        /// OnGet for PrivacyModel
+        /// </summary>
         public void OnGet()
         {
         }
