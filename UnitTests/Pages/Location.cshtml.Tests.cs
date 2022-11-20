09e948ec058d10a64ef6a02027a7ba5ab32ed25f
parth560 <114316487+parth560@users.noreply.github.com>
2022-11-19 17:04:49 -0800

    Added the unit testing for "Location.chtml.cs" page under Pages folder.

---
+++ UnitTests/Pages/Location.cshtml.Tests.cs
@@ -0,0 +1,57 @@
+using Microsoft.Extensions.Logging;
+using NUnit.Framework;
+using Moq;
+using ContosoCrafts.WebSite.Pages;
+
+namespace UnitTests.Pages.Location
+{
+    /// <summary>
+    /// Tests Payment page
+    /// </summary>
+    public class LocationTests
+    {
+        #region TestSetup
+
+        // Variable for PaymentModel pageModel
+        public static LocationModel pageModel;
+
+        /// <summary>
+        /// Initialises the initial state
+        /// </summary>
+        [SetUp]
+        public void TestInitialize()
+        {
+            // Variable for mocklogger
+            var MockLoggerDirect = Mock.Of<ILogger<LocationModel>>();
+
+            pageModel = new LocationModel(MockLoggerDirect)
+            {
+                PageContext = TestHelper.PageContext,
+                TempData = TestHelper.TempData,
+            };
+        }
+
+        #endregion TestSetup
+
+        #region OnGet
+
+        /// <summary>
+        /// OnGetif valid activity set, should return requestId
+        /// </summary>
+        [Test]
+        public void OnGet_Valid_Activity_Set_Should_Return_RequestId()
+        {
+            // Arrange
+
+            // Act
+            pageModel.OnGet();
+
+            // Reset
+
+            // Assert
+            Assert.AreEqual(true, pageModel.ModelState.IsValid);
+        }
+
+        #endregion OnGet
+    }
+}
--- src/Pages/Location.cshtml.cs
+++ src/Pages/Location.cshtml.cs
@@ -1,55 +1,30 @@
-using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
-using ContosoCrafts.WebSite.Models;
-using ContosoCrafts.WebSite.Services;
 
 namespace ContosoCrafts.WebSite.Pages
 {
     /// <summary>
-    /// Location page shows location of the seller
+    /// Payment page
     /// </summary>
     public class LocationModel : PageModel
     {
-        // Logger variable
+        // Variable for logger
         private readonly ILogger<LocationModel> _logger;
 
         /// <summary>
-        /// Location model mockLogger
-        /// </summary>
-        /// <param name="mockLoggerDirect"></param>
-        public LocationModel(ILogger<LocationModel> mockLoggerDirect)
-        {
-        }
-
-        /// <summary>
-        /// Constructor LocationModel logger cariable instantiation
+        /// Constructor for PaymentModel
         /// </summary>
         /// <param name="logger"></param>
-        /// <param name="productService"></param>
-        public LocationModel(ILogger<LocationModel> logger,
-            JsonFileProductService productService)
+        public LocationModel(ILogger<LocationModel> logger)
         {
             _logger = logger;
-            ProductService = productService;
         }
 
         /// <summary>
-        /// Getter for JsonFileProductService ProductService 
-        /// </summary>
-        public JsonFileProductService ProductService { get; }
-
-        /// <summary>
-        /// Getter setter for Products
-        /// </summary>
-        public IEnumerable<ProductModel> Products { get; private set; }
-
-        /// <summary>
-        /// Gets all the data
+        /// OnGet for payments
         /// </summary>
         public void OnGet()
         {
-            Products = ProductService.GetAllData();
         }
     }
 }
