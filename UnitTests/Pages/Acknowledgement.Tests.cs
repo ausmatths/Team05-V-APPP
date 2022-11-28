0ccf308af02530c787d0f985f162c372f15a6cee
parth560 <114316487+parth560@users.noreply.github.com>
2022-11-27 20:58:51 -0800

    Added unit testing for "Acknowledgemnt.Tests.cs" page under UnitTests/Pages folder

---
+++ UnitTests/Pages/Acknowledgement.Tests.cs
@@ -0,0 +1,57 @@
+using Microsoft.Extensions.Logging;
+using NUnit.Framework;
+using Moq;
+using ContosoCrafts.WebSite.Pages;
+
+namespace UnitTests.Pages.Acknowledgement
+{
+    /// <summary>
+    /// Tests Payment page
+    /// </summary>
+    public class PaymentTests
+    {
+        #region TestSetup
+
+        // Variable for AcknowledgementModel pageModel
+        public static AcknowledgementModel pageModel;
+
+        /// <summary>
+        /// Initialises the initial state
+        /// </summary>
+        [SetUp]
+        public void TestInitialize()
+        {
+            // Variable for mocklogger
+            var MockLoggerDirect = Mock.Of<ILogger<AcknowledgementModel>>();
+
+            pageModel = new AcknowledgementModel(MockLoggerDirect)
+            {
+                PageContext = TestHelper.PageContext,
+                TempData = TestHelper.TempData,
+            };
+        }
+
+        #endregion TestSetup
+
+        #region OnGet
+
+        /// <summary>
+        /// OnGetif valid activity set, should return requestId
+        /// </summary>
+        [Test]
+        public void OnGet_Valid_Activity_Set_Should_Return_RequestId()
+        {
+            // Arrange
+
+            // Act
+            pageModel.OnGet();
+
+            // Reset
+
+            // Assert
+            Assert.AreEqual(true, pageModel.ModelState.IsValid);
+        }
+
+        #endregion OnGet
+    }
+}
