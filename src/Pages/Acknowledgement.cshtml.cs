14cabe437e59b1a6c1fc255ed453646216373190
parth560 <114316487+parth560@users.noreply.github.com>
2022-11-27 20:38:53 -0800

    Added "Acknowledgement page" for payment confirmation under Pages folder

---
+++ src/Pages/Acknowledgement.cshtml.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace ContosoCrafts.WebSite.Pages
+{
+    /// <summary>
+    /// Acknowledgement page for Thank you description
+    /// </summary>
+    public class AcknowledgementModel : PageModel
+    {
+        // Variable for logger
+        private readonly ILogger<AcknowledgementModel> _logger;
+
+        /// <summary>
+        /// Constructor for AcknowledgementModel
+        /// </summary>
+        /// <param name="logger"></param>
+        public AcknowledgementModel(ILogger<AcknowledgementModel> logger)
+        {
+            _logger = logger;
+        }
+
+        /// <summary>
+        /// OnGet for PrivacyModel
+        /// </summary>
+        public void OnGet()
+        {
+        }
+    }
+}
