4d58812e565e3d9e2502fcb514b2ecf9f894ee89
ptaneti <ptaneti@seattleu.edu>
2022-11-28 00:05:21 -0800

    Added "Recommendations" file under "Pages" Folder

---
+++ src/Pages/Recommendations.cshtml.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace ContosoCrafts.WebSite.Pages
+{
+    /// <summary>
+    /// Recommendation page
+    /// </summary>
+    public class RecommendationsModel : PageModel
+    {
+        // Variable for logger
+        private readonly ILogger<RecommendationsModel> _logger;
+
+        /// <summary>
+        /// Constructor for RecommendationsModel
+        /// </summary>
+        /// <param name="logger"></param>
+        public RecommendationsModel(ILogger<PrivacyModel> logger)
+        {
+            _logger = _logger;
+        }
+
+        /// <summary>
+        /// OnGet for RecommendationsModel
+        /// </summary>
+        public void OnGet()
+        {
+        }
+    }
+}
