2bc26bcd21f89bfacf76044ff866df27c295a806
Priya Bhardwaj <pbhardwaj@seattleu.edu>
2022-11-29 22:42:35 -0800

    Removed extra lines of code from Recommendations.cshtml.cs

--- src/Pages/Recommendations.cshtml.cs
+++ src/Pages/Recommendations.cshtml.cs
@@ -8,23 +8,6 @@ namespace ContosoCrafts.WebSite.Pages
     /// </summary>
     public class RecommendationsModel : PageModel
     {
-        // Variable for logger
-        private readonly ILogger<RecommendationsModel> _logger;
-
-        /// <summary>
-        /// Constructor for RecommendationsModel
-        /// </summary>
-        /// <param name="logger"></param>
-        public RecommendationsModel(ILogger<PrivacyModel> logger)
-        {
-            _logger = _logger;
-        }
-
-        /// <summary>
-        /// OnGet for RecommendationsModel
-        /// </summary>
-        public void OnGet()
-        {
-        }
+       
     }
 }
